1d901dba7eb94b35cb897b03958c7e913abec7ac
Tunbosun Ayinla <tayinla@gmail.com>
2020-07-26 12:23:20 +0100

    Update the currency symbol for South African Rand (ZAR)

--- includes/class-wc-gateway-paystack.php
+++ includes/class-wc-gateway-paystack.php
@@ -252,7 +252,7 @@ class WC_Gateway_Paystack extends WC_Payment_Gateway_CC {
 
 		if ( ! in_array( get_woocommerce_currency(), apply_filters( 'woocommerce_paystack_supported_currencies', array( 'NGN', 'USD', 'ZAR', 'GHS' ) ) ) ) {
 
-			$this->msg = sprintf( __( 'Paystack does not support your store currency. Kindly set it to either NGN (&#8358), GHS (&#x20b5;), USD (&#36;) or ZAR (&#163;) <a href="%s">here</a>', 'woo-paystack' ), admin_url( 'admin.php?page=wc-settings&tab=general' ) );
+			$this->msg = sprintf( __( 'Paystack does not support your store currency. Kindly set it to either NGN (&#8358), GHS (&#x20b5;), USD (&#36;) or ZAR (R) <a href="%s">here</a>', 'woo-paystack' ), admin_url( 'admin.php?page=wc-settings&tab=general' ) );
 
 			return false;
 
