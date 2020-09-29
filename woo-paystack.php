9a9a7e22f74a1600428da475be8b3c508195fa19
Tunbosun Ayinla <tubiz@users.noreply.github.com>
2020-09-29 11:49:49 +0100

    Issue/30 - Test mode admin notice not displayed properly in WP 5.5 (#31)
    Fixes: #30

--- woo-paystack.php
+++ woo-paystack.php
@@ -152,6 +152,6 @@ function tbz_wc_paystack_testmode_notice() {
 
 	if ( 'yes' === $test_mode ) {
 		/* translators: 1. Paystack settings page URL link. */
-		echo '<div class="update-nag">' . sprintf( __( 'Paystack test mode is still enabled, Click <strong><a href="%s">here</a></strong> to disable it when you want to start accepting live payment on your site.', 'woo-paystack' ), esc_url( admin_url( 'admin.php?page=wc-settings&tab=checkout&section=paystack' ) ) ) . '</div>';
+		echo '<div class="error"><p>' . sprintf( __( 'Paystack test mode is still enabled, Click <strong><a href="%s">here</a></strong> to disable it when you want to start accepting live payment on your site.', 'woo-paystack' ), esc_url( admin_url( 'admin.php?page=wc-settings&tab=checkout&section=paystack' ) ) ) . '</p></div>';
 	}
 }
