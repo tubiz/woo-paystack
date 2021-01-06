65cb6cc69414786de36c80cd51708987d9352c27
lukman-paystack <58217917+lukman-paystack@users.noreply.github.com>
2021-01-06 10:05:32 +0100

    add option to autocomplete order after payment (#40)

--- includes/class-wc-gateway-custom-paystack.php
+++ includes/class-wc-gateway-custom-paystack.php
@@ -219,6 +219,15 @@ class WC_Gateway_Custom_Paystack extends WC_Gateway_Paystack_Subscriptions {
 				'default'     => 'no',
 				'desc_tip'    => true,
 			),
+			'autocomplete_order'                  => array(
+				'title'       => __( 'Autocomplete order after payment', 'woo-paystack' ),
+				'label'       => __( 'Autocomplete order', 'woo-paystack' ),
+				'type'        => 'checkbox',
+				'class'       => 'wc-paystack-autocomplete-order',
+				'description' => __( 'If enabled, the order will be marked as complete after successful payment', 'woo-paystack' ),
+				'default'     => 'no',
+				'desc_tip'    => true,
+			),
 		);
 
 	}
