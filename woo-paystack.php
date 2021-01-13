73966bd5e405696e460fb1c57b000dac6b48dc8f
Tunbosun Ayinla <tayinla@gmail.com>
2021-01-13 10:51:07 +0100

    Bump version to 5.7

--- woo-paystack.php
+++ woo-paystack.php
@@ -3,13 +3,13 @@
  * Plugin Name: Paystack WooCommerce Payment Gateway
  * Plugin URI: https://paystack.com
  * Description: WooCommerce payment gateway for Paystack
- * Version: 5.6.5
+ * Version: 5.7
  * Author: Tunbosun Ayinla
  * Author URI: https://bosun.me
  * License: GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  * WC requires at least: 3.0.0
- * WC tested up to: 4.6
+ * WC tested up to: 4.9
  * Text Domain: woo-paystack
  * Domain Path: /languages
  */
@@ -21,7 +21,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 define( 'WC_PAYSTACK_MAIN_FILE', __FILE__ );
 define( 'WC_PAYSTACK_URL', untrailingslashit( plugins_url( '/', __FILE__ ) ) );
 
-define( 'WC_PAYSTACK_VERSION', '5.6.4' );
+define( 'WC_PAYSTACK_VERSION', '5.7' );
 
 /**
  * Initialize Paystack WooCommerce payment gateway.
