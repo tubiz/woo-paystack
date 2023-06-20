6417a5dede6f08b10830389194e6cc225911ca7b
Tunbosun Ayinla <tayinla@gmail.com>
2023-06-20 10:16:07 +0100

    PHPCS fix

--- woo-paystack.php
+++ woo-paystack.php
@@ -187,8 +187,11 @@ function tbz_wc_paystack_testmode_notice() {
 	$note->save();
 }
 
-add_action( 'before_woocommerce_init', function () {
-	if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
-		\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
+add_action(
+	'before_woocommerce_init',
+	function () {
+		if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
+			\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
+		}
 	}
-} );
+);
