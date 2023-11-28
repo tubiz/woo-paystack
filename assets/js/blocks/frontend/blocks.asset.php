7da185a963e8f3bea9c7facb980054899de44097
Tunbosun Ayinla <tayinla@gmail.com>
2023-11-28 09:43:26 +0100

    Recompile assets

--- assets/js/blocks/frontend/blocks.asset.php
+++ assets/js/blocks/frontend/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '56849d74fd092513b4dd');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-html-entities', 'wp-i18n'), 'version' => '4c764c6a3000337eb113');
---
+++ assets/js/blocks/frontend/blocks/paystack-five.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-html-entities', 'wp-i18n'), 'version' => 'd3243b6728f8e007d06d');
